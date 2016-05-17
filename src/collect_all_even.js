56e7ab970e989a0fdc4dfab30179e84d9ebba1bf
李杨帆 <2531349593@qq.com>
2016-05-17 16:40:31 +0800

    four

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -2,7 +2,6 @@ function collect_same_elements(collection_a, object_b) {
     var result = [];
 
     for (var i = 0; i < collection_a.length; i++) {
-        //for (var j = 0; j < object_b.value.length; j++)
         object_b.value.forEach(function (object) {
             if (collection_a[i].key == object) {
                 result.push(collection_a[i].key);
